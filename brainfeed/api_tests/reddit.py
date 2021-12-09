c2a412a15835cdf8723b645fc67a48ce48f6e164
Eric Chang <ChiHsun@Chi-Hsuns-MacBook-Pro.local>
2021-12-09 15:23:43 -0500

    experimenting how to extract and store posts information

--- brainfeed/api_tests/reddit.py
+++ brainfeed/api_tests/reddit.py
@@ -1,15 +1,37 @@
 import praw
 from pprint import pprint
+import pandas as pd
 
 # Initialize reddit instance
 reddit = praw.Reddit("brainfeed_script")
-reddit.config.user_agent
-reddit.config.username
+print(reddit.config.user_agent)
+print(reddit.config.username)
 
-# Stream subreddit submissions
+# Get submissions with a certain keyword and store the information in a dataframe
+posts_brain = []
+subreddits = reddit.subreddit("science")
+for submission in subreddits.search("brain"):
+    posts_brain.append([submission.title, submission.score, submission.id, submission.subreddit, submission.url, submission.num_comments, submission.selftext, submission.created_utc])
+posts_brain = pd.DataFrame(posts_brain, columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created_time'])
+posts_brain["created_time"] = pd.to_datetime(posts_brain['created_time'], unit='s') # convert the UTC timestamp to readable date
+posts_brain.head()
+
+# Get existing submissions
+# this is impossible now as Subreddit.submissions() has been removed 
+
+# Get new submissions and store the information in a dataframe
+submissions = []
 subreddits = reddit.subreddit("science")
 for submission in subreddits.stream.submissions():
-    print(submission.title)
+    submissions.append([submission.title, submission.score, submission.id, submission.subreddit, submission.url, submission.num_comments, submission.selftext, submission.created_utc])
+submissions = pd.DataFrame(submissions, columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created_time'])
+submissions.head()
+
+# # Stream subreddit submissions
+# subreddits = reddit.subreddit("science")
+# for submission in subreddits.stream.submissions():
+#     print(submission.title)
+#     print()
 
 # Testing
 submission.url
